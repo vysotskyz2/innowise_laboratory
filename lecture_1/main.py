b1cf1bd57b96c8b5cc93d7f641cfd7322cd8b59c
reigen137~ <nikita.vysotsky@mail.ru>
2025-11-13 22:59:15 +0300

    main.py

---
+++ lecture_1/main.py
@@ -0,0 +1,7 @@
+from colorama import init, Fore, Back, Style
+
+init()
+print(f"{Fore.RED}{Back.YELLOW}Hello World!{Style.RESET_ALL}")
+print(f"{Fore.GREEN}Hello World in Green!{Style.RESET_ALL}")
+print(f"{Fore.BLUE}{Style.BRIGHT}Hello World in Bright Blue!{Style.RESET_ALL}")
+print(f"{Fore.MAGENTA}{Back.CYAN}Hello World with Magenta text and Cyan background!{Style.RESET_ALL}")
